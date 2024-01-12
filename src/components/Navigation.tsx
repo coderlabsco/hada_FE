94288660d121f6c3091cd276a082ef9843014053
César Fontalvo <cesar@coderlabs.co>
2024-01-12 15:25:49 -0500

    change:: deletes unused lines

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,8 +2,7 @@ import navBarIcon from "../assets/navbarIcon.png"
 import Image from "next/image"
 const Navigation = () => {
   return (
-    <nav className="flex items-center justify-between p-4 bg-[#fafafa]  text-white">
-      {/* <p className="flex items-center font-bold"></p> */}
+    <nav className="flex items-center justify-between p-4 bg-[#fafafa]">
       <Image src={navBarIcon} alt="Logo"/>
       <button className=" text-black py-2 px-4 rounded ease-in-out duration-300">
         Name
