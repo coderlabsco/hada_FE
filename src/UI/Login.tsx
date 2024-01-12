e4b26d74936b1ec71949c07cc5bd7e718f8f39f7
Sebastian <sebastian@coderlabs.co>
2024-01-12 11:28:58 -0500

    feat:: add login //TODO implement context

--- src/UI/Login.tsx
+++ src/UI/Login.tsx
@@ -1,79 +1,77 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 'use client'
-import type React from 'react'
 import { Button, Checkbox, Form, Input, notification } from 'antd'
 import { useMutation } from '@apollo/client'
 import { LOGIN } from '../../types/operation'
-import { getClient } from '@/graphql/ApolloClient'
+import { useRouter } from 'next/navigation'
 
 function App() {
-
-  const onFinish = async (values: any) => {
-    const { data } = await getClient().mutate({
-      mutation: LOGIN,
-      variables: { loginUserInput: { ...values } },
+    const router = useRouter()
+    const [login, {loading, data}] = useMutation(LOGIN, {
+      onCompleted: (props) => {
+        console.log(props);
+        router.push('/history')
+      }
     })
-
-    console.log(data);
+  const onFinish = async (values: any) => {
+    login({variables: {loginUserInput: values}})
+    
   }
-  
-  
-  
+
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo)
   }
-  
+
   interface FieldType {
-    username?: string
+    email?: string
     password?: string
     remember?: string
   }
 
-  
   return (
     <>
-    <h2 className="text-xl font-semibold mb-[22px]">Ingresar Como Vigilante</h2>
-    <Form
-      name="signIn"
-      layout="vertical"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      requiredMark={false}
-      style={{ width: '100%' }}
+      <h2 className="text-xl font-semibold mb-[22px]">Ingresar Como Vigilante</h2>
+      <Form
+        name="signIn"
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        requiredMark={false}
+        style={{ width: '100%' }}
       >
-      <Form.Item<FieldType>
-        label="Correo"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+        <Form.Item<FieldType>
+          label="Correo"
+          name="email"
+          rules={[{ required: true, message: 'Please input your username!' }]}
         >
-        <Input size="large" placeholder='correo electrónico' />
-      </Form.Item>
+          <Input size="large" placeholder='correo electrónico' />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        label="Contraseña"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
+        <Form.Item<FieldType>
+          label="Contraseña"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
         >
-        <Input.Password size="large" placeholder='Contraseña' />
-      </Form.Item>
+          <Input.Password size="large" placeholder='Contraseña' />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        name="remember"
-        valuePropName="checked"
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
         >
-        <Checkbox style={{ color: '#444444', fontSize: 14 }}>Recordarme</Checkbox>
-      </Form.Item>
+          <Checkbox style={{ color: '#444444', fontSize: 14 }}>Recordarme</Checkbox>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" block size="large">
-          Sign In
-        </Button>
-      </Form.Item>
-    </Form>
-  </>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block size="large">
+            Sign In
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
 
-)
+  )
 }
 
 
