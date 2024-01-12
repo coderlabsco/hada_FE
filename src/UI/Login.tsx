100f1b711a6703307beba367f572190f491da5ae
César Fontalvo <cesar@coderlabs.co>
2024-01-12 15:24:02 -0500

    change:: updates types

--- src/UI/Login.tsx
+++ src/UI/Login.tsx
@@ -6,31 +6,27 @@ import { LOGIN } from '../../types/operation'
 import { useRouter } from 'next/navigation'
 
 function App() {
-    const router = useRouter()
-    const [login, {loading, data}] = useMutation(LOGIN, {
-      onCompleted: (props) => {
-        console.log(props);
-        router.push('/history')
-      }
-    })
-  const onFinish = async (values: any) => {
-    login({variables: {loginUserInput: values}})
-    
+  const router = useRouter()
+
+  const [login, { loading }] = useMutation(LOGIN, {
+    onCompleted: (credentials) => {
+      console.log(credentials)
+      router.push('/history')
+    },
+  })
+  const onFinish = (values: any) => {
+    login({ variables: { loginUserInput: values } })
   }
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo)
   }
 
-  interface FieldType {
-    email?: string
-    password?: string
-    remember?: string
-  }
-
   return (
     <>
-      <h2 className="text-xl font-semibold mb-[22px]">Ingresar Como Vigilante</h2>
+      <h2 className="text-xl font-semibold mb-[22px]">
+        Ingresar Como Vigilante
+      </h2>
       <Form
         name="signIn"
         layout="vertical"
@@ -45,7 +41,7 @@ function App() {
           name="email"
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Input size="large" placeholder='correo electrónico' />
+          <Input size="large" placeholder="correo electrónico" />
         </Form.Item>
 
         <Form.Item<FieldType>
@@ -53,26 +49,34 @@ function App() {
           name="password"
           rules={[{ required: true, message: 'Please input your password!' }]}
         >
-          <Input.Password size="large" placeholder='Contraseña' />
+          <Input.Password size="large" placeholder="Contraseña" />
         </Form.Item>
 
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-        >
-          <Checkbox style={{ color: '#444444', fontSize: 14 }}>Recordarme</Checkbox>
+        <Form.Item<FieldType> name="remember" valuePropName="checked">
+          <Checkbox style={{ color: '#444444', fontSize: 14 }}>
+            Recordarme
+          </Checkbox>
         </Form.Item>
 
         <Form.Item>
-          <Button type="primary" htmlType="submit" block size="large">
+          <Button
+            loading={loading}
+            type="primary"
+            htmlType="submit"
+            block
+            size="large"
+          >
             Sign In
           </Button>
         </Form.Item>
       </Form>
     </>
-
   )
 }
 
-
 export default App
+type FieldType = {
+  email?: string
+  password?: string
+  remember?: string
+}
