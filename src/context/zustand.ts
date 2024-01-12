f20f688a844bdb784d6b6fdbc9d3ac0bd9f40f27
César Fontalvo <cesar@coderlabs.co>
2024-01-12 18:09:07 -0500

    change:: adds types to zustand store

--- src/context/zustand.ts
+++ src/context/zustand.ts
@@ -1,8 +1,18 @@
 import {create} from 'zustand'
+import {persist} from 'zustand/middleware'
 
-export const useStore = create((set: any) => ({
+type useStoreTypes = {
+    access_token: string
+    refresh_token: string
+    setAccess_token: (access: string) => void
+    setRefresh_token: () => void
+}
+
+export const useStore = create<useStoreTypes>((set: any) => (
+  {
     access_token: "",
     refresh_token: "",
-    setAccess_token: (access: string) => set((state: any) => ({access_token: access})), 
+    setAccess_token: (access) => set((state: any) => ({access_token: access})), 
     setRefresh_token: () => set((state: any) => ({access_token: state.refresh_token})), 
-}))
+  }
+))
