a8d4a5d4ca03caa439ceabcda29a5b29f4cf36e7
César Fontalvo <cesar@coderlabs.co>
2024-01-16 15:43:59 -0500

    add:: creates component for protected route

---
+++ src/app/login/template.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useLayoutEffect } from 'react'
+import { usePathname, redirect } from 'next/navigation'
+import { useLoginStore } from '@/store/zustand'
+
+export default function Template({ children }: { children: JSX.Element }): JSX.Element {
+  const authed = useLoginStore(state => state.access_token)
+
+  useLayoutEffect(() => {
+    if (authed) {
+      redirect('/history')
+    }
+  }, [authed])
+
+  return children
+}
