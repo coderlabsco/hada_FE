a094efd2ace6b228d388894b301cdafba61890f8
César Fontalvo <cesar@coderlabs.co>
2024-01-16 14:50:24 -0500

    change:: renames store folder

--- src/store/zustand.ts
+++ src/store/zustand.ts
@@ -3,16 +3,19 @@ import {persist} from 'zustand/middleware'
 
 type useStoreTypes = {
     access_token: string
+    name: string
     refresh_token: string
     setAccess_token: (access: string) => void
     setRefresh_token: () => void
+    newName: (name: string) => void
 }
 
-export const useStore = create<useStoreTypes>((set: any) => (
-  {
+export const useLoginStore = create<useStoreTypes>()((set) => ({
     access_token: "",
+    name: 'No name',
     refresh_token: "",
-    setAccess_token: (access) => set((state: any) => ({access_token: access})), 
+    setAccess_token: (access: string) => set((state: any) => ({access_token: access})), 
     setRefresh_token: () => set((state: any) => ({access_token: state.refresh_token})), 
-  }
-))
+    newName: (name: string) => {set( (state) => ({name}))},
+  }))
+  
