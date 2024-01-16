d3fb861af60de7c0f53d492061dc01bd043ad055
César Fontalvo <cesar@coderlabs.co>
2024-01-16 14:56:16 -0500

    change:: adds persist for local storage

--- src/store/zustand.ts
+++ src/store/zustand.ts
@@ -3,19 +3,20 @@ import {persist} from 'zustand/middleware'
 
 type useStoreTypes = {
     access_token: string
-    name: string
     refresh_token: string
     setAccess_token: (access: string) => void
     setRefresh_token: () => void
-    newName: (name: string) => void
 }
 
-export const useLoginStore = create<useStoreTypes>()((set) => ({
+export const useLoginStore = create( 
+    persist<useStoreTypes>(
+    (set) => ({
     access_token: "",
-    name: 'No name',
     refresh_token: "",
     setAccess_token: (access: string) => set((state: any) => ({access_token: access})), 
     setRefresh_token: () => set((state: any) => ({access_token: state.refresh_token})), 
-    newName: (name: string) => {set( (state) => ({name}))},
-  }))
+  }), {
+    name: 'credentials'
+  })
+  )
   
