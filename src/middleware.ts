00d37617084becd365127d367e73f88834e9b5da
César Fontalvo <cesar@coderlabs.co>
2024-01-16 15:44:20 -0500

    change:: updates logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,17 @@ const protectedRoutes = ['/history']
 const loggedIn = true
 
 export default function middleware(req: any) {
-    const isLoggedIn = useLoginStore.getState()
-    console.log("isLoggedIn", isLoggedIn)
-    // const isProtected = protectedRoutes.includes(req.nextUrl.pathname)
-    // if(!isLoggedIn && isProtected) {
-    if(!loggedIn) {
-        const absoluteURL = new URL('/', req.nextUrl.origin) 
-        return NextResponse.redirect(absoluteURL.toString())
-    }
+    // const isLoggedIn = useLoginStore.getState()
+    // console.log("isLoggedIn", isLoggedIn)
+    // // const isProtected = protectedRoutes.includes(req.nextUrl.pathname)
+    // // if(!isLoggedIn && isProtected) {
+    // if(!loggedIn) {
+    //     const absoluteURL = new URL('/', req.nextUrl.origin) 
+    //     return NextResponse.redirect(absoluteURL.toString())
+    // }
     return NextResponse.next()
 }
 
-export const config = {
-    matcher: ['/history/:path*']
-} 
+// export const config = {
+//     matcher: ['/history/:path*']
+// } 
