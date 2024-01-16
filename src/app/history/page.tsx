d3c14ff052edd1eac7aeec79697330d7770f60b7
César Fontalvo <cesar@coderlabs.co>
2024-01-16 15:44:10 -0500

    change:: updates component name

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,6 +1,6 @@
 import {Flex} from 'antd'
 import HistoryUI from '@/UI/History'
-export default function History (): JSX.Element {
+export default function Page (): JSX.Element {
     return (
         <section className='relative flex min-h-screen flex-col items-center justify-center'>
             <Flex vertical justify='center'>
